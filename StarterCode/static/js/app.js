2af2b64e34727ea482fa7cfa5c0eb6a60ff37053
B-the-Reed <b.richard.reed@gmail.com>
2023-07-09 10:33:58 -0600

    finishing the assignment

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,130 +1,43 @@
 
 const bellyData = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
-// THIS INIT() FUNCTION AND THE D3 PART IS WORKING, BUT I'D LIKE TO GET IT INTO A FUNCTION FORM INSTEAD. 
+// We start by creating a function that initializes with the first value in the dropdown list (OTU ID 940) as the default. 
 
-// function init(){
+function main(){
 
-//     d3.json(bellyData).then(function(data)
-//     {
+    let userSelection = d3.select("#selDataset")
 
-//     let selID = data.samples.filter(object => object.id === "940")
+    d3.json(bellyData).then((data) => {
+        let dropdown_list = data.names;
 
-//     console.log("What is selID?", selID)
-
-//     let selectID = selID[0]
-
-//     console.log("This is now selectID", selectID)
-
-//     let otuList = []
-//     for (let i = 0; i < 10; i++){
-//         otuList.push(`OTU ${selectID.otu_ids[i]}`)
-//     };
-
-//     let sampleValList = []
-//     for (let i = 0; i < 10; i++){
-//         sampleValList.push(selectID.sample_values[i])
-//     };
-
-//     let otuLabelList = []
-//     for (let i = 0; i < 10; i++){
-//         otuLabelList.push(selectID.otu_labels[i])
-//     };
-
-
-//     console.log(`OTU List: ${otuList}`)
-//     console.log(`Sample Value List: ${sampleValList}`)
-//     console.log(`OTU Labels: ${otuLabelList}`)
-
-//     let trace = {
-//         x: sampleValList.map(object => object).reverse(),
-//         y: otuList.map(object => object).reverse(),
-//         type: "bar",
-//         orientation: "h",
-//         hovertext: otuLabelList.map(object => object).reverse() // this isn't pulling in the otu labels...
-//     };
-
-//     let myData = [trace];
-
-//     let layout = {
-//         title: "OTU Frequency"
-//     };
-
-//     Plotly.newPlot("plot", myData, layout)
-
-// });
-
-// }
-
-
-// init();
-
-
-
-
-// d3.json(bellyData).then(function(data){
+        for (let i=0; i<dropdown_list.length; i++){
+            userSelection.append("option").text(dropdown_list[i]).property("value", dropdown_list[i]);
+        };
     
-//     let idNames = Object.values(data.names)
-//     console.log("ids", idNames);
+    let defaultValue = dropdown_list[0];
 
-//     d3.select("#selDataset").on("click", function(){    
-//         let dropdown = d3.select("#selDataset")
-//         let choice = dropdown.property("value");
-//         console.log("selected option", choice)
-
-//         idNames.forEach(function(option) {
-//             dropdown.append("option").text(option)});
+    barChart(defaultValue)
+    bubbleChart(defaultValue);
+    metadata(defaultValue)
     
+    });
+};
 
-//         let selID = data.samples.filter(object => object.id === choice)
-
-//         let selectID = selID[0]
-        
-//         console.log(selectID)
-        
-//         let otuList = []
-//         for (let i = 0; i < 10; i++){
-//             otuList.push(`OTU ${selectID.otu_ids[i]}`)
-//         };
-        
-//         let sampleValList = []
-//         for (let i = 0; i < 10; i++){
-//             sampleValList.push(selectID.sample_values[i])
-//         };
-        
-//         let otuLabelList = []
-//         for (let i = 0; i < 10; i++){
-//             otuLabelList.push(selectID.otu_labels[i])
-//         };
-        
-        
-//         // console.log(`OTU List: ${otuList}`)
-//         // console.log(`Sample Value List: ${sampleValList}`)
-//         // console.log(`OTU Labels: ${otuLabelList}`)
-        
-//         let trace = {
-//             x: sampleValList.map(object => object).reverse(),
-//             y: otuList.map(object => object).reverse(),
-//             type: "bar",
-//             orientation: "h",
-//             hovertext: otuLabelList.map(object => object).reverse() // this isn't pulling in the otu labels...
-//         };
-        
-//         let myData = [trace];
-        
-//         let layout = {
-//             title: "OTU Frequency"
-//         };
-        
-//         Plotly.newPlot("plot", myData, layout)
-
-//     });
-// });
+main();
 
 
-// TRYING THE FUNCTION VERSION...
+// Using this optionChanged function to accept the newInput as whatever the user selects from the dropdown menu. 
+// That value will then be used in each of the three relevant functions: barChart, bubbleChart, and metadata. 
 
 
+function optionChanged(newInput){
+    barChart(newInput),
+    bubbleChart(newInput),
+    metadata(newInput)
+};
+
+
+// Building the bar chart. 
 
 function barChart(sample){
     
@@ -150,24 +63,22 @@ function barChart(sample){
         for (let i = 0; i < 10; i++){
             otuLabelList.push(selectID.otu_labels[i])
         };
-            
-            
-            // console.log(`OTU List: ${otuList}`)
-            // console.log(`Sample Value List: ${sampleValList}`)
-            // console.log(`OTU Labels: ${otuLabelList}`)
-            
+                        
         let trace = {
             x: sampleValList.map(object => object).reverse(),
             y: otuList.map(object => object).reverse(),
             type: "bar",
             orientation: "h",
-            hovertext: otuLabelList.map(object => object).reverse() // this isn't pulling in the otu labels...
+            hovertext: otuLabelList.map(object => object).reverse()
         };
             
         let myData = [trace];
             
         let layout = {
-            title: "OTU Frequency"
+            title: "Top 10 OTUs in Individual",
+            xaxis: {
+                title: "Sample Value"
+            }
         };
             
         Plotly.newPlot("bar", myData, layout)
@@ -176,52 +87,11 @@ function barChart(sample){
 };
 
 
-function main(){
-
-    let userSelection = d3.select("#selDataset")
-
-    d3.json(bellyData).then((data) => {
-        let dropdown_list = data.names;
-
-        for (let i=0; i<dropdown_list.length; i++){
-            userSelection.append("option").text(dropdown_list[i]).property("value", dropdown_list[i]);
-        };
-    
-    let defaultValue = dropdown_list[0];
-
-    barChart(defaultValue)
-    bubbleChart(defaultValue);
-    
-    });
-};
-
-
-function optionChanged(newInput){
-    barChart(newInput)
-    bubbleChart(newInput)
-};
-
-
-
-main();
-
-
+// Building the bubble chart. 
 
 function bubbleChart(sample){
     
-    // Create a bubble chart that displays each sample.
-
-    // Use otu_ids for the x values.
-
-    // Use sample_values for the y values.
-
-    // Use sample_values for the marker size.
-
-    // Use otu_ids for the marker colors.
-
-    // Use otu_labels for the text values.
-
-    d3.json(bellyData).then(function(data){
+     d3.json(bellyData).then(function(data){
 
         let selID = data.samples.filter(object => object.id === sample)
 
@@ -237,12 +107,15 @@ function bubbleChart(sample){
 
         console.log("y-axis", sample_values)
 
+        var desired_maximum_marker_size = 40;
+
         var trace1 = {
         x: otuIDs,
         y: sample_values,
         mode: 'markers',
         marker: {
-          size: 10
+          size: sample_values,
+          color: otuIDs
         }
       };
       
@@ -251,8 +124,14 @@ function bubbleChart(sample){
       var layout = {
         // title: 'Marker Size',
         showlegend: false,
-        height: 600,
-        width: 600
+        // height: 600,
+        // width: 600,
+        xaxis: {
+            title: "OTU ID"
+        },
+        yaxis: {
+            title: "Sample Value"
+        }
       };
       
       Plotly.newPlot("bubble", myData, layout);
@@ -263,6 +142,67 @@ function bubbleChart(sample){
 };
 
 
-function metadata(newInput){
 
-};
+// For the metadata, need to display each key-value pair: 
+// {
+//     "id": 940,
+//     "ethnicity": "Caucasian",
+//     "gender": "F",
+//     "age": 24.0,
+//     "location": "Beaufort/NC",
+//     "bbtype": "I",
+//     "wfreq": 2.0
+// }
+
+
+// Populating the metadata. 
+
+function metadata(sample){
+    d3.json(bellyData).then(function(data){
+        
+        console.log("sample", sample)
+
+        let selID = data.metadata.filter(object => object.id === Number(sample))
+
+        console.log("selID", selID)
+
+        let selectID = selID[0]
+
+        console.log(selectID)
+
+        var demographics = Object.entries(selectID);
+
+        d3.selectAll('p').remove();
+
+        d3.select('#sample-metadata').selectAll('p').data(demographics).enter().append('p').text(d => {
+            return `${d[0]}: ${d[1]}`
+        })
+
+        console.log(demographics)
+
+        // let demoKeys = Object.keys(selectID)
+
+        // let demoValues = Object.values(selectID)
+        
+        // console.log("object keys", demoKeys)
+        // console.log("object values", demoValues)
+        
+        // // Doing this all manually -- need to make this more dynamic...
+        // let demoInfo = document.getElementById("sample-metadata");
+        // demoInfo.innerHTML = `${demoKeys[0]}: ${demoValues[0]} <br> ${demoKeys[1]}: ${demoValues[1]}` 
+
+        // var Metadata=data.metadata;
+        // Metadata.forEach(person => {
+        //     if (person.id ==value){
+        //         var demographics = Object.entries(person);
+        //         wash = demographics[6][1];
+        //         d3.selectAll('p').remove();
+        //         d3.select('#sample-metadata').selectAll('p').data(demographics).enter().append('p').text(d=>{
+        //             return `${d[0]}: ${d[1]}`;
+        //         });
+        //     }
+        // });
+
+
+    }
+)};
